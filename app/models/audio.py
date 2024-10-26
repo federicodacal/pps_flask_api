630ac04b3252ef5709b3337a784e080ee0b87aca
Federico Dacal <federicodacal@gmail.com>
2024-10-26 19:34:32 -0300

    routes CRUD audio

--- app/models/audio.py
+++ app/models/audio.py
@@ -1,3 +1,4 @@
+import datetime
 from sqlalchemy import func
 from pps_flask_api.app import db
 
@@ -19,3 +20,41 @@ class Audio(db.Model):
     description = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     modified_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, ID, creator_ID, file_name, audio_name, state, category, genre, BPM,
+                 tone, length, size, description, score=None, created_at=None, modified_at=None):
+        self.ID = ID
+        self.creator_ID = creator_ID
+        self.file_name = file_name
+        self.audio_name = audio_name
+        self.state = state
+        self.category = category
+        self.genre = genre
+        self.BPM = BPM
+        self.tone = tone
+        self.length = length
+        self.size = size
+        self.description = description
+        self.score = score
+        self.created_at = created_at or datetime.datetime.now(datetime.timezone.utc)
+        self.modified_at = modified_at or datetime.datetime.now(datetime.timezone.utc)
+
+    # Método para representar el objeto como un diccionario
+    def to_dict(self):
+        return {
+            "ID": self.ID,
+            "creator_ID": self.creator_ID,
+            "file_name": self.file_name,
+            "audio_name": self.audio_name,
+            "state": self.state,
+            "category": self.category,
+            "genre": self.genre,
+            "BPM": self.BPM,
+            "tone": self.tone,
+            "length": self.length,
+            "size": self.size,
+            "score": self.score,
+            "description": self.description,
+            "created_at": self.created_at,
+            "modified_at": self.modified_at,
+        }
--- app/routes/audio_routes.py
+++ app/routes/audio_routes.py
@@ -8,24 +8,94 @@ audio_routes = Blueprint('audio_routes', __name__)
 # GET ALL
 @audio_routes.route('/audios')
 def get_audios():
-    return 'Listado Audios'
+    audios = Audio.query.all()
+    return jsonify([audio.to_dict() for audio in audios]), 200
 
 # GET BY ID
 @audio_routes.route('/audios/<string:audio_id>', methods=['GET'])
 def get_audio(audio_id):
-    return 'Audio ID:' + audio_id
+    audio = Audio.query.get(audio_id)
+    if not audio:
+        return {"error": "Audio no encontrado"}, 404
+    return jsonify(audio.to_dict()), 200
+
+# GET BY CREATOR ID
+@audio_routes.route('/audios/creator/<string:creator_id>', methods=['GET'])
+def get_audios_by_creator(creator_id):
+    audios = Audio.query.filter_by(creator_ID=creator_id).all()
+    if audios:
+        return jsonify([audio.to_dict() for audio in audios]), 200
+    else:
+        return {"error": "No se encontraron audios para este creador"}, 404
 
 # CREATE
 @audio_routes.route('/audios', methods=['POST'])
 def create_audio():
-    return 'Create Audio'
+
+    data = request.json or {}
+
+    if not data:
+        return {"error": "Datos no proporcionados"}, 400
+
+    new_audio = Audio(
+        ID=data['ID'],
+        creator_ID=data['creator_ID'],
+        file_name=data['file_name'],
+        audio_name=data['audio_name'],
+        state=data['state'],
+        category=data['category'],
+        genre=data['genre'],
+        BPM=data['BPM'],
+        tone=data['tone'],
+        length=data['length'],
+        size=data['size'],
+        score=data.get('score'),  # Opcional
+        description=data['description'],
+        created_at=datetime.datetime.now(datetime.timezone.utc),
+        modified_at=datetime.datetime.now(datetime.timezone.utc)
+    )
+    db.session.add(new_audio)
+    db.session.commit()
+    return jsonify(new_audio.to_dict()), 201
 
 # UPDATE
 @audio_routes.route('/audios/<string:audio_id>', methods=['PUT'])
 def update_audio(audio_id):
-    return 'Update Audio'
+
+    data = request.json or {}
+
+    if not data:
+        return {"error": "Datos no proporcionados"}, 400
+
+    audio = Audio.query.get(audio_id)
+    if not audio:
+        return {"error": "Audio no encontrado"}, 404
+
+    audio.file_name = data.get('file_name', audio.file_name)
+    audio.audio_name = data.get('audio_name', audio.audio_name)
+    audio.state = data.get('state', audio.state)
+    audio.category = data.get('category', audio.category)
+    audio.genre = data.get('genre', audio.genre)
+    audio.BPM = data.get('BPM', audio.BPM)
+    audio.tone = data.get('tone', audio.tone)
+    audio.length = data.get('length', audio.length)
+    audio.size = data.get('size', audio.size)
+    audio.score = data.get('score', audio.score)
+    audio.description = data.get('description', audio.description)
+    audio.modified_at = datetime.datetime.now(datetime.timezone.utc)
+
+    db.session.commit()
+    return jsonify(audio.to_dict()), 200
 
 # DELETE
 @audio_routes.route('/audios/<string:audio_id>', methods=['DELETE'])
 def delete_audio(audio_id):
-    return 'Delete Audio'
+
+    audio = Audio.query.get(audio_id)
+
+    if not audio:
+        return {"error": "Audio no encontrado"}, 404
+
+    db.session.delete(audio)
+    db.session.commit()
+    return {"message": "Audio eliminado exitosamente"}, 200
