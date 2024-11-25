8c7369ebca0cecd03710b494f17fef005b00a90e
Federico Dacal <federicodacal@gmail.com>
2024-11-25 09:53:24 -0300

    refactor auth service

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,46 +1,13 @@
-from datetime import timedelta
 from flask import jsonify, request
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
-
-from ..services.user_service import UserService
 from ..services.auth_service import AuthService
 
 class AuthController:
 
     @staticmethod
     def login():
-        #response = AuthService.create_token()
-
         data = request.json or {}
-
-        if not data:
-            return {"message": "No se recibieron los datos"}, 400
-        
-        if not data.get("email") or data["email"] == '':
-            return {"message": "No se ingresó el email"}, 400
-        
-        if not data.get("pwd") or data["pwd"] == '':
-            return {"message": "No se ingresó la clave"}, 400
-        
-        email = data["email"]
-        password = data["pwd"]
-
-        user = UserService.get_user_by_email(email)
-        
-        if user is None:
-            return {"message": f"Usuario con email {email} no encontrado"}, 404
-
-        if not password == user["pwd"]:
-            return {"message": "No autorizado"}, 401
-        else:
-            expires_in = timedelta(minutes=30)
-            token = create_access_token(identity=email, expires_delta=expires_in)
-            return {
-                "message": "Token creado con éxito",
-                "token": token,
-                "user": user,
-                "status_code": 200
-            }, 200
+        response, status_code = AuthService.create_token(data)
+        return jsonify(response), status_code
     
     @staticmethod
     def logout():
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,16 +1,40 @@
-from flask import jsonify, request
-from flask_jwt_extended import jwt_required, get_jwt, verify_jwt_in_request
+from datetime import timedelta
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, verify_jwt_in_request
+from ..services.user_service import UserService
 from ..utils.token_manager import revoke_token
 
 class AuthService:
 
     @staticmethod
-    def create_token():
-        return 'Login service'
-    
-    @staticmethod
-    def register():
-        return 'Register service'
+    def create_token(data):
+        if not data:
+            return {"message": "No se recibieron los datos"}, 400
+        
+        if not data.get("email") or data["email"] == '':
+            return {"message": "No se ingresó el email"}, 400
+        
+        if not data.get("pwd") or data["pwd"] == '':
+            return {"message": "No se ingresó la clave"}, 400
+        
+        email = data["email"]
+        password = data["pwd"]
+
+        user = UserService.get_user_by_email(email)
+        
+        if user is None:
+            return {"message": f"Usuario con email {email} no encontrado"}, 404
+
+        if not password == user["pwd"]:
+            return {"message": "No autorizado"}, 401
+        else:
+            expires_in = timedelta(minutes=30)
+            token = create_access_token(identity=email, expires_delta=expires_in)
+            return {
+                "message": "Token creado con éxito",
+                "token": token,
+                "user": user,
+                "status_code": 200
+            }, 200
     
     @staticmethod
     @jwt_required()
@@ -18,6 +42,9 @@ class AuthService:
         verify_jwt_in_request()
         jti = get_jwt()["jti"]
         revoke_token(jti)
-        print("\nSe cancela el token\n")
         return {"message": "Token cancelado con éxito"}, 200
+    
+    @staticmethod
+    def register():
+        return 'Register service'
     
