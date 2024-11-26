5f5a5d0ad239a79c6945299ecccc68a6d5ed9bad
Federico Dacal <federicodacal@gmail.com>
2024-11-26 05:28:37 -0300

    audio controller send file

--- app/controllers/audio_controller.py
+++ app/controllers/audio_controller.py
@@ -17,19 +17,19 @@ class AudioController:
     
     @staticmethod 
     def get_audio_file(audio_file_name):
+        '''
         audio_file = AudioService.get_audio_file_from_gridfs(audio_file_name)
 
         if not audio_file:
             return {"error": "Archivo no encontrado en MongoDB"}, 404
-
+        
         file_size = audio_file.length
         start = 0
         end = file_size - 1
 
-        # Leer el encabezado Range para transmisión parcial
         range_header = request.headers.get('Range', None)
         if range_header:
-            range_match = re.search(r'bytes=(\d+)-(\d*)', range_header)
+            
             if range_match:
                 start = int(range_match.group(1))
                 if range_match.group(2):
@@ -38,24 +38,26 @@ class AudioController:
         chunk_size = (end - start) + 1
         audio_file.seek(start)
 
+    
         def generate():
             remaining = chunk_size
             while remaining > 0:
-                chunk = audio_file.read(min(8192, remaining))
+                chunk = audio_file.read(min(8192, remaining))  
                 if not chunk:
                     break
                 remaining -= len(chunk)
                 yield chunk
 
         return Response(
-            stream_with_context(generate()),
-            status=206,  # Indica transmisión parcial
+            generate(),
+            status=206,  # Transmisión parcial
             mimetype='audio/mpeg',
             headers={
                 "Content-Range": f"bytes {start}-{end}/{file_size}",
                 "Accept-Ranges": "bytes",
                 "Content-Length": str(chunk_size),
                 "Content-Disposition": f"inline; filename={audio_file.filename}",
+                "Transfer-Encoding": "chunked",
             },
         )
         '''
@@ -70,7 +72,7 @@ class AudioController:
             as_attachment=True, 
             download_name=audio_file.filename
         ), 200
-        '''
+        
         
     @staticmethod
     def get_audios_by_creator(creator_id):
